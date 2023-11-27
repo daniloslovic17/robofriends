56cfd1b7dfdf951893b7b349f70d08d0cdd4fb7a
daniloslovic17 <daniloslovic17@gmail.com>
2023-11-27 13:45:26 +0100

    Adding this.state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox"
 import { robots } from './robots'; // {} is used because export in this file isn't default, if there was for example another variable cats it would be {robots, cats}
 
 
-const App = () => {
-	return(
-		<div className='tc' >
-			<h1>Robofriends</h1>
-			<SearchBox />
-			<CardList robots = { robots } />
-		</div>
-	);
+class App extends Component {
+	constructor(){
+		super(); // In order to use this, we have to call 'super'. States can change props and affect the app.
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+
+	render(){
+		return(
+			<div className='tc' >
+				<h1>Robofriends</h1>
+				<SearchBox />
+				<CardList robots = { this.state.robots } />
+			</div>
+		);
+	}
 }
 
 export default App;
