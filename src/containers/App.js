e83b494970ca839bc9af1a6bf5174c46de7ba9a8
daniloslovic17 <daniloslovic17@gmail.com>
2023-11-28 12:45:18 +0100

    Adding ErroBoundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry"
 import './App.css';
 // import { robots } from './robots'; // {} is used because export in this file isn't default, if there was for example another variable cats it would be {robots, cats}
 // (not required anymore due to fetch part)
@@ -39,7 +40,9 @@ class App extends Component {
 					<h1 className='f1'>Robofriends</h1>
 					<SearchBox searchChange = { this.onSearchChange } />
 					<Scroll>
-						<CardList robots = { filteredRobots } />
+						<ErrorBoundry>
+							<CardList robots = { filteredRobots } />
+						</ErrorBoundry>
 					</Scroll>
 				</div>
 			);
