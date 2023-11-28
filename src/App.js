04c9bf34c9c4cfce30e1c493cf591edd3b96fe0d
daniloslovic17 <daniloslovic17@gmail.com>
2023-11-28 11:27:29 +0100

    Fetch and componentDidMount

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox"
 import './App.css';
-import { robots } from './robots'; // {} is used because export in this file isn't default, if there was for example another variable cats it would be {robots, cats}
-
+// import { robots } from './robots'; // {} is used because export in this file isn't default, if there was for example another variable cats it would be {robots, cats}
+// (not required anymore due to fetch part)
 
 class App extends Component {
 	constructor(){
 		super(); // In order to use this, we have to call 'super'. States can change props and affect the app.
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	// This is part of React, that's why we don't use arrow function
+	// Part of lifecycle methods
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users').then( response => response.json()).then( users => this.setState({ robots: users }))
 	}
 
 	onSearchChange = (event) => {
@@ -23,13 +29,17 @@ class App extends Component {
 				return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 			} )
 
-		return(
-			<div className='tc' >
-				<h1 className='f1'>Robofriends</h1>
-				<SearchBox searchChange = { this.onSearchChange } />
-				<CardList robots = { filteredRobots } />
-			</div>
-		);
+		if (this.state.robots.length === 0){
+			return <h1 className='tc'>Loading</h1>
+		} else {
+			return(
+				<div className='tc' >
+					<h1 className='f1'>Robofriends</h1>
+					<SearchBox searchChange = { this.onSearchChange } />
+					<CardList robots = { filteredRobots } />
+				</div>
+			);
+		}
 	}
 }
 
