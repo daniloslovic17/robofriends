f47faaef389804832767712bac0f03eaa9fa1d68
daniloslovic17 <daniloslovic17@gmail.com>
2023-11-28 12:02:42 +0100

    Folder structures

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import './App.css';
 // import { robots } from './robots'; // {} is used because export in this file isn't default, if there was for example another variable cats it would be {robots, cats}
 // (not required anymore due to fetch part)
@@ -26,11 +26,12 @@ class App extends Component {
 	}
 
 	render(){
-		const filteredRobots = this.state.robots.filter( robot => {
-				return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter( robot => {
+				return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 			} )
 
-		if (this.state.robots.length === 0){
+		if ( !robots.length ){
 			return <h1 className='tc'>Loading</h1>
 		} else {
 			return(
