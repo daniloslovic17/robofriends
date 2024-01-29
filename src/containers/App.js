f279dd36900df8dd8e13348ff57181a1b7c5f42e
daniloslovic17 <daniloslovic17@gmail.com>
2024-01-29 12:50:55 +0100

    React Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
@@ -7,51 +7,41 @@ import './App.css';
 // import { robots } from './robots'; // {} is used because export in this file isn't default, if there was for example another variable cats it would be {robots, cats}
 // (not required anymore due to fetch part)
 
-class App extends Component {
-	constructor(){
-		super(); // In order to use this, we have to call 'super'. States can change props and affect the app.
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
+const App = () => {
+	const[robots, setRobots] = useState([]);
+	const[searchfield, setSearchfield] = useState('');
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users').then( response => response.json()).then( users => {setRobots(users)})
+	}, []);
+
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value);
 	}
+	const filteredRobots = robots.filter( robot => {
+			if(robot.name.toLowerCase().includes(searchfield.toLowerCase())){
+				return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+			} else if(robot.email.toLowerCase().includes(searchfield.toLowerCase())){
+				return robot.email.toLowerCase().includes(searchfield.toLowerCase());
+			} else {
+				return null;
+			}
+		} )
 
-	// This is part of React, that's why we don't use arrow function
-	// Part of lifecycle methods
-	componentDidMount(){
-		fetch('https://jsonplaceholder.typicode.com/users').then( response => response.json()).then( users => this.setState({ robots: users }))
-	}
-
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value });
-	}
-
-	render(){
-		const { robots, searchfield } = this.state;
-		const filteredRobots = robots.filter( robot => {
-				if(robot.name.toLowerCase().includes(searchfield.toLowerCase())){
-					return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-				}
-				if(robot.email.toLowerCase().includes(searchfield.toLowerCase())){
-					return robot.email.toLowerCase().includes(searchfield.toLowerCase());
-				}
-			} )
-
-		if ( !robots.length ){
-			return <h1 className='tc'>Loading</h1>
-		} else {
-			return(
-				<div className='tc' >
-					<h1 className='f1'>Robofriends</h1>
-					<SearchBox searchChange = { this.onSearchChange } />
-					<Scroll>
-						<ErrorBoundry>
-							<CardList robots = { filteredRobots } />
-						</ErrorBoundry>
-					</Scroll>
-				</div>
-			);
-		}
+	if ( !robots.length ){
+		return <h1 className='tc'>Loading</h1>
+	} else {
+		return(
+			<div className='tc' >
+				<h1 className='f1'>Robofriends</h1>
+				<SearchBox searchChange = { onSearchChange } />
+				<Scroll>
+					<ErrorBoundry>
+						<CardList robots = { filteredRobots } />
+					</ErrorBoundry>
+				</Scroll>
+			</div>
+		);
 	}
 }
 
